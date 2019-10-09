e6f1bcfeddb17114c11c804d03a6cb78d1b431d2
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-10-09 13:35:25 +1300

    Min and Max int values

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,10 @@ public class Main {
 
         int myValue = 10000;
 
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
     }
 }
